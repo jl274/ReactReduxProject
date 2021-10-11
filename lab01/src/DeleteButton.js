1a274e1af8431a6bbbd446787f070a7e28b4c3d9
Jakub Lendzinski <j.lendzinski.390@studms.edu.ug.pl>
2021-10-11 21:41:49 +0200

    wyniesie logiki usuwania poza element product list do własnego komponentu

---
+++ lab01/src/DeleteButton.js
@@ -0,0 +1,79 @@
+import axios from "axios";
+
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faTrashAlt } from '@fortawesome/free-regular-svg-icons';
+
+import { useEffect, useState } from "react";
+
+import Button from '@mui/material/Button';
+import Dialog from '@mui/material/Dialog';
+import DialogActions from '@mui/material/DialogActions';
+import DialogContent from '@mui/material/DialogContent';
+import DialogContentText from '@mui/material/DialogContentText';
+import DialogTitle from '@mui/material/DialogTitle';
+
+const element = <FontAwesomeIcon icon={faTrashAlt} />
+
+const url = "https://fakestoreapi.com/"
+
+const DeleteButton = (props) => {
+
+    // deleting product
+    const deleteProduct = async (id) => {
+        try {
+            const deletionResult = await axios.delete(url + "products/" + id);
+            if (deletionResult.status === 200){
+                props.delete(id);
+            }
+        } catch (error) {
+            console.error(error);
+        }
+    }
+
+    // opening confirmation
+    const [activeId, setActiveId] = useState(null);
+    const [open, setOpen] = useState(false);
+
+    const handleClickOpen = () => {
+        setOpen(true);
+    };
+
+    const handleClose = () => {
+        setOpen(false);
+    };
+
+    return(
+        <div>
+            <div 
+                className="delete"
+                onClick={async () => {
+                setActiveId(props.id);
+                handleClickOpen();
+                }}
+                >{element}</div>
+                <Dialog
+                    open={open}
+                    onClose={handleClose}
+                    aria-labelledby="alert-dialog-title"
+                    aria-describedby="alert-dialog-description"
+                >
+                    <DialogTitle id="alert-dialog-title">
+                        {"Are you sure you want to delete this product?"}
+                    </DialogTitle>
+                    <DialogContent>
+                    <DialogContentText id="alert-dialog-description">
+                        This action is permanent!
+                    </DialogContentText>
+                    </DialogContent>
+                    <DialogActions>
+                    <Button onClick={handleClose}>Cancel</Button>
+                    <Button onClick={()=>{deleteProduct(activeId);handleClose();}} autoFocus>
+                        Delete
+                    </Button>
+                    </DialogActions>
+                </Dialog>
+            </div>
+    )
+}
+
+export default DeleteButton;
--- lab01/src/ProductList.js
+++ lab01/src/ProductList.js
@@ -1,18 +1,11 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
+import DeleteButton from "./DeleteButton";
 import './styles/ProductList.scss';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faTrashAlt } from '@fortawesome/free-regular-svg-icons';
-
-import Button from '@mui/material/Button';
-import Dialog from '@mui/material/Dialog';
-import DialogActions from '@mui/material/DialogActions';
-import DialogContent from '@mui/material/DialogContent';
-import DialogContentText from '@mui/material/DialogContentText';
-import DialogTitle from '@mui/material/DialogTitle';
 
 
-const element = <FontAwesomeIcon icon={faTrashAlt} />
+
+
 
 const url = "https://fakestoreapi.com/"
 
@@ -40,29 +33,7 @@ const ProductList = (props) => {
         fetchData();
     }, []);
 
-    // deleting product
-    const deleteProduct = async (id) => {
-        try {
-            const deletionResult = await axios.delete(url + "products/" + id);
-            if (deletionResult.status === 200){
-                props.delete(id);
-            }
-        } catch (error) {
-            console.error(error);
-        }
-    }
-
-    // opening confirmation
-    const [activeId, setActiveId] = useState(null);
-    const [open, setOpen] = useState(false);
-
-    const handleClickOpen = () => {
-        setOpen(true);
-    };
-
-    const handleClose = () => {
-        setOpen(false);
-    };
+    
 
 
     return (
@@ -74,35 +45,7 @@ const ProductList = (props) => {
                             <img src={elem.image} alt="Product"></img>
                             <p>{elem.title}</p>
                             <p>{elem.price}$</p>
-                            <div 
-                                className="delete"
-                                onClick={async () => {
-                                    setActiveId(elem.id);
-                                    handleClickOpen();
-                                    // deleteProduct(elem.id);
-                                }}
-                            >{element}</div>
-                            <Dialog
-                                open={open}
-                                onClose={handleClose}
-                                aria-labelledby="alert-dialog-title"
-                                aria-describedby="alert-dialog-description"
-                            >
-                                <DialogTitle id="alert-dialog-title">
-                                {"Are you sure you want to delete this product?"}
-                                </DialogTitle>
-                                <DialogContent>
-                                <DialogContentText id="alert-dialog-description">
-                                    This action is permanent!
-                                </DialogContentText>
-                                </DialogContent>
-                                <DialogActions>
-                                <Button onClick={handleClose}>Cancel</Button>
-                                <Button onClick={()=>{deleteProduct(activeId);handleClose();}} autoFocus>
-                                    Delete
-                                </Button>
-                                </DialogActions>
-                            </Dialog>
+                            <DeleteButton delete={props.delete} id={elem.id}/>
                         </div>
                     </li>)}
             </ul>
