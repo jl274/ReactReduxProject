54ea32423f6c35ffd9a8b75c16a3f4c4b3f58004
Jakub Lendzinski <j.lendzinski.390@studms.edu.ug.pl>
2021-10-17 12:36:27 +0200

    zad5 commit?2

--- lab01/src/ProductDetail.js
+++ lab01/src/ProductDetail.js
@@ -1,15 +1,30 @@
-import { Redirect, withRouter } from "react-router";
+import { withRouter } from "react-router";
+import './styles/ProductDetail.scss';
 
 const ProductDetails = (props) => {
 
     const givenId = props.match.params.id;
 
-    const productInfo = props.appUserList.filter(x => parseInt(x.id) === parseInt(givenId))
+    const productInfo = props.appUserList.filter(x => parseInt(x.id) === parseInt(givenId))[0]
     console.log(productInfo)
 
     return (
+        <div>
+        <h3>{productInfo.title}</h3>
         <div className="detailbox">
-            {givenId}
+            
+            <div className="left">
+                <img src={`${productInfo.image}`} alt={`${productInfo.title}`}/>
+            </div>
+            <div className="right">
+                <ul>
+                    <li>Rating: {productInfo.rating.rate}</li>
+                    <li>{productInfo.price}</li>
+                    <li>{productInfo.category}</li>
+                    <li>{productInfo.description}</li>
+                </ul>
+            </div>
+        </div>
         </div>
     )
 }
