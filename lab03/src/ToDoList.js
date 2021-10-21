65103c567502d23b9b81dc246d164f34f944a127
Jakub Lendzinski <j.lendzinski.390@studms.edu.ug.pl>
2021-10-21 14:21:23 +0200

    Z1C3 usprawanienia form

--- lab03/src/ToDoList.js
+++ lab03/src/ToDoList.js
@@ -1,7 +1,7 @@
 import { connect } from "react-redux"
 
 export const toDoListReducer = (state = [
-    {id: 1, name: "abc", date:"2020/10/10", done: false}
+    {id: 1, name: "abc", date: new Date("2020/10/10"), done: false}
 ], action) => {
 
     switch(action.type){
@@ -14,14 +14,16 @@ const ToDoList = ({ stateToDoList },props) => {
 
 
     return (
-        <div>
+        <div style={{margin: "10px 30vw"}}>
             <ul>
                 {stateToDoList.map(todo => <li key={todo.id}>
-                    Id: {todo.id}
-                    Name: {todo.name}
-                    Date: {todo.date}
-                    Done: {todo.done.toString()}
+                    <ul>
+                        <li>Id: {todo.id}</li>
+                        <li>Name: {todo.name}</li>
+                        <li>Date: {todo.date.toLocaleDateString()}</li>
+                        <li>Done: {todo.done.toString()}</li>
                     {console.log(todo)}
+                    </ul>
                 </li>)}
             </ul>
         </div>
--- lab03/src/TodoForm.js
+++ lab03/src/TodoForm.js
@@ -1,11 +1,34 @@
 import { connect } from "react-redux";
 import { Field, Formik } from 'formik';
+import {v4 as uuidv4} from 'uuid';
 
-export const toDoFormReducer = (state = [], action) => {
+export const toDoFormReducer = (state = {}, action) => {
     switch(action.type){
+
+        case 'ADD_TODO':
+            return {...state, ...action.payload}
+
         default:
             return state
     }
+}
+
+const addTodo = (id, name, date) => {
+    return {
+        type: 'ADD_TODO',
+        payload: {
+            id,
+            name,
+            date: new Date(date),
+            done: false
+        }
+    }
+}
+
+const initialValues = {
+    id: uuidv4(),
+    name: '',
+    date: '',
 }
 
 const ToDoForm = () => {
@@ -14,16 +37,14 @@ const ToDoForm = () => {
     return (
         <div>
             <Formik
-                initialValues={{
-                    name: '',
-                    date: '',
-                }}
+                initialValues={initialValues}
                 onSubmit={(vals)=>console.log(vals)}
             >
                 {({errors}) => (
-                    <form>
-                        <Field type="text" name="name"></Field>
+                    <form style={{display: "flex", alignItems: "center", justifyContent: "center", margin: "10px"}}>
+                        <Field type="text" name="name" placeholder="ToDo name"></Field>
                         <Field type="date" name="date"></Field>
+                        <button type="submit">Dodaj</button>
                     </form>
                 )}
             </Formik>
