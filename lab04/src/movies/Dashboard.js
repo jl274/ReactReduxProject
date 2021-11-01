8b00efc77cae745a84e1bb9421281aba06bd4316
Jakub Lendzinski <j.lendzinski.390@studms.edu.ug.pl>
2021-11-01 23:32:22 +0100

    Dodane obsługę "pustej" bazy filmów

--- lab04/src/movies/Dashboard.js
+++ lab04/src/movies/Dashboard.js
@@ -12,20 +12,22 @@ const Dashboard = ({directors, movies}, props) => {
     return (
         <div className="dashboard">
             <h3>Newest Movies</h3>
-            <div className="movieList">
-                    <ul>
-                        <li>Title</li>
-                        <li>Year</li>
-                        <li>Director</li>
-                    </ul>
-                {movies.sort((a,b)=>a.creationDate - b.creationDate).reverse().slice(0,3).map(movie => 
-                    <ul key={movie.id}>
-                        <li>{movie.title}</li>
-                        <li>{movie.productionYear}</li>
-                        <li>{movie.director}</li>
-                    </ul>)
-                }
-            </div>
+            {movies.length === 0 ? <p>No movies yet</p> : 
+                <div className="movieList">
+                        <ul>
+                            <li>Title</li>
+                            <li>Year</li>
+                            <li>Director</li>
+                        </ul>
+                    {movies.sort((a,b)=>a.creationDate - b.creationDate).reverse().slice(0,3).map(movie => 
+                        <ul key={movie.id}>
+                            <li>{movie.title}</li>
+                            <li>{movie.productionYear}</li>
+                            <li>{movie.director}</li>
+                        </ul>)
+                    }
+                </div>
+            }
         </div>
     )
 }
--- lab04/src/movies/MovieList.js
+++ lab04/src/movies/MovieList.js
@@ -7,20 +7,22 @@ const MovieList = ({movies}, props) => {
     return (
         <div className="dashboard">
             <h3>Movie List</h3>
-            <div className="movieList">
-                    <ul>
-                        <li>Title</li>
-                        <li>Year</li>
-                        <li>Director</li>
-                    </ul>
-                {movies.sort((a,b)=>a.creationDate - b.creationDate).reverse().map(movie => 
-                    <ul key={movie.id}>
-                        <li>{movie.title}</li>
-                        <li>{movie.productionYear}</li>
-                        <li>{movie.director}</li>
-                    </ul>)
+            {movies.length === 0 ? <p>No movies yet</p> : 
+                <div className="movieList">
+                        <ul>
+                            <li>Title</li>
+                            <li>Year</li>
+                            <li>Director</li>
+                        </ul>
+                    {movies.sort((a,b)=>a.creationDate - b.creationDate).reverse().map(movie => 
+                        <ul key={movie.id}>
+                            <li>{movie.title}</li>
+                            <li>{movie.productionYear}</li>
+                            <li>{movie.director}</li>
+                        </ul>)
+                    }
+                </div>
                 }
-            </div>
         </div>
     )
 }
