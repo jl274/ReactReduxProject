f2f109f09fb4ef6deb89ba2df7c910df666fbab1
Jakub Lendzinski <j.lendzinski.390@studms.edu.ug.pl>
2021-11-15 09:54:05 +0100

    Added top movies and top actors to dashboard + minor fixes

--- lab04/src/movies/Dashboard.js
+++ lab04/src/movies/Dashboard.js
@@ -4,32 +4,74 @@ import '../styles/movieTable.scss';
 
 const Dashboard = ({directors, movies, actors}, props) => {
 
-    console.log("Directors");
-    console.log(directors);
-    console.log("Movies");
-    console.log(movies);
-    console.log("Actors");
-    console.log(actors);
+
+    // I leave it here in order to future tests and debugging
+    // console.log("Directors");
+    // console.log(directors);
+    // console.log("Movies");
+    // console.log(movies);
+    // console.log("Actors");
+    // console.log(actors);
 
     return (
-        <div className="dashboard">
-            <h3>Newest Movies</h3>
+        <div>
+            <div className="dashboard">
+                <h3>Newest Movies</h3>
+                {movies.length === 0 ? <p>No movies yet</p> : 
+                    <div className="movieList">
+                            <ul>
+                                <li>Title</li>
+                                <li>Year</li>
+                                <li>Director</li>
+                            </ul>
+                        {movies.sort((a,b)=>a.creationDate - b.creationDate).reverse().slice(0,3).map(movie => 
+                            <ul key={movie.id}>
+                                <li>{movie.title}</li>
+                                <li>{movie.productionYear}</li>
+                                <li>{movie.director}</li>
+                            </ul>)
+                        }
+                    </div>
+                }
+            </div>
+            <div className="dashboard">
+            <h3>Top movies</h3>
             {movies.length === 0 ? <p>No movies yet</p> : 
-                <div className="movieList">
+                <div className="topList">
                         <ul>
                             <li>Title</li>
-                            <li>Year</li>
-                            <li>Director</li>
+                            {/* <li>Director</li> */}
+                            <li>Actors number</li>
                         </ul>
-                    {movies.sort((a,b)=>a.creationDate - b.creationDate).reverse().slice(0,3).map(movie => 
+                    {movies.sort((a,b)=>a.actors.length- b.actors.length).reverse().slice(0,3).map(movie => 
                         <ul key={movie.id}>
                             <li>{movie.title}</li>
-                            <li>{movie.productionYear}</li>
-                            <li>{movie.director}</li>
+                            {/* <li>{movie.director}</li> */}
+                            <li>{movie.actors.length}</li>
                         </ul>)
                     }
                 </div>
             }
+            </div>
+            <div className="dashboard">
+            <h3>Top actors</h3>
+            {movies.length === 0 ? <p>No movies yet</p> : 
+                <div className="topList">
+                        <ul>
+                            <li>Name</li>
+                            {/* <li>Director</li> */}
+                            <li>Movies number</li>
+                        </ul>
+                    {actors.sort((a,b)=>a.movies.length- b.movies.length).reverse().slice(0,3).map(actor => 
+                        <ul key={actor.id}>
+                            <li>{actor.lastName} {actor.firstName}</li>
+                            {/* <li>{movie.director}</li> */}
+                            <li>{actor.movies.length}</li>
+                        </ul>)
+                    }
+                </div>
+            }
+            </div>
         </div>
     )
 }
