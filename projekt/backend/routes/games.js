7005d2f01efa5c6931c5cbb41f599f21bb62a10d
Jakub Lendzinski <j.lendzinski.390@studms.edu.ug.pl>
2021-12-16 16:49:23 +0100

    Basic PUT for producers and games

--- projekt/backend/routes/games.js
+++ projekt/backend/routes/games.js
@@ -23,7 +23,7 @@ router.post('/', async (req, res) => {
 
         try {
 
-            const exists = await Producer.find({_id: idProducer});
+            const exists = await Producer.exists({_id: idProducer});
 
             if (exists) {
 
@@ -36,6 +36,42 @@ router.post('/', async (req, res) => {
 
             } else {
                 return res.status(404).json({err: "Producer not found"})
+            }
+
+        } catch (err) {
+            return res.status(500).json({err})
+        }
+
+    } else {
+        return res.status(400).json({err: "Missing arguments"})
+    }
+
+})
+
+router.put('/:id', async (req, res) => {
+
+    const { idProducer, name, genre, playingTime, url, complexity, minAge } = req.body;
+    const { id } = req.params;
+
+    if (idProducer && name && genre && complexity){
+
+        try {
+
+            const exists = await Producer.exists({_id: idProducer});
+            const game_exists = await Game.exists({_id: id})
+
+            if (exists && game_exists) {
+
+                await Game.updateOne({_id: id}, {
+                    name, genre, playingTime, complexity, minAge, 
+                    producer: idProducer, offers: [], url
+                });
+                const game = await Game.find({_id: id});
+
+                return res.json({game})
+
+            } else {
+                return res.status(404).json({err: "Producer or game not found"})
             }
 
         } catch (err) {
