40b4d23cf9171f13524265e8856b177c210764cf
Jakub Lendzinski <j.lendzinski.390@studms.edu.ug.pl>
2021-12-19 19:03:35 +0100

    internationalization init

--- projekt/webapp/src/App.js
+++ projekt/webapp/src/App.js
@@ -1,7 +1,43 @@
 import logo from './logo.svg';
 import './App.css';
+import i18next from 'i18next';
+import { initReactI18next /*,useTranslation*/ } from 'react-i18next';
+import Backend from 'i18next-http-backend';
+import languages from './config/languages';
+
+// internationalization ------------
+const language = languages.find(value => value === localStorage.getItem('language'));
+
+i18next.use(Backend)
+  // .use(LanguageDetector)
+  .use(initReactI18next)
+  .init({
+    lng: language || 'pl',
+    fallbackLng: 'en',
+    ns: [ 'main' ],
+    defaultNS: 'main',
+    react: {
+      wait: true,
+      useSuspense: false
+    },
+    interpolation: {
+      escapeValue: false
+    },
+    backend: {
+      loadPath: '/locales/{{lng}}/{{ns}}.json'
+    }
+})
+
 
 function App() {
+
+  // const { t, i18n } = useTranslation();
+
+  // const changeLanguage = (lng) => {
+  //   i18n.changeLanguage(lng);
+  //   localStorage.setItem('language', lng);
+  // }
+
   return (
     <div className="App">
       <header className="App-header">
