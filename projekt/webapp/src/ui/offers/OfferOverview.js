c35e5191296a714b483aa2a4aa37ffc5bcd1cfbd
Jakub Lendzinski <j.lendzinski.390@studms.edu.ug.pl>
2021-12-27 16:40:48 +0100

    Transaltion integration + style imporvement

--- projekt/webapp/src/ui/offers/OfferOverview.js
+++ projekt/webapp/src/ui/offers/OfferOverview.js
@@ -1,10 +1,13 @@
 import '../../styles/Overview.scss';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faStoreAlt } from '@fortawesome/free-solid-svg-icons';
+import { useTranslation } from 'react-i18next';
 
 const shopIcon = <FontAwesomeIcon icon={faStoreAlt}/>
 
 const OfferOverview = ({data, imgUrl}) => {
+
+    const { t } = useTranslation();
     
     return (
         <div className="overview">
@@ -16,10 +19,10 @@ const OfferOverview = ({data, imgUrl}) => {
                     <img src={imgUrl} alt="Miniature cover"></img>
                 </li>
                 <li>
-                    Price: <p>{parseFloat(data.price)}</p>,-PLN
+                    {t('offerOverview.price')}<p>{parseFloat(data.price)}</p>,-PLN
                 </li>
                 <li>
-                    <a href={data.link} target="_blank" rel="noopener noreferrer" ><button type='submit'>Go to offer</button></a>
+                    <a href={data.link} target="_blank" rel="noopener noreferrer" ><button type='submit'>{t('offerOverview.button')}</button></a>
                 </li>
             </ul>
         </div>
