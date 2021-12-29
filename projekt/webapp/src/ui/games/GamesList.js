44242ac00005f6087f96659bc25e8b2bd22a26ff
Jakub Lendzinski <j.lendzinski.390@studms.edu.ug.pl>
2021-12-29 20:00:51 +0100

    Basic games filtering

--- projekt/webapp/src/ui/games/GamesList.js
+++ projekt/webapp/src/ui/games/GamesList.js
@@ -6,6 +6,8 @@ import { getAllProducers } from '../../ducks/producers/selectors';
 import { Link } from 'react-router-dom'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faPlus } from '@fortawesome/free-solid-svg-icons';
+import _ from 'lodash';
+import { useState } from 'react';
 
 const plus = <FontAwesomeIcon icon={faPlus} />
 
@@ -23,17 +25,59 @@ const GamesList = ({games, producers}) => {
         }
     }
 
+    const [sortMethod, setSortMethod] = useState("new");
+
+    const gamesToMap = () => {
+        let games_copy = [...games]
+        if (sortMethod === "new"){
+            games_copy = _.reverse(games_copy)
+        } else if (sortMethod === "leastTime"){
+            games_copy = _.sortBy(games_copy, ['playingTime']);
+        } else if (sortMethod === "mostTime"){
+            games_copy = _.reverse(_.sortBy(games_copy, ['playingTime']));
+        } else if (sortMethod === "leastComplex"){
+            games_copy = _.sortBy(games_copy, ['complexity']);
+        } else if (sortMethod === "mostComplex"){
+            games_copy = _.reverse(_.sortBy(games_copy, ['complexity']));
+        } else if (sortMethod === "az"){
+            games_copy = _.sortBy(games_copy, ['name']);
+        } else if (sortMethod === "za"){
+            games_copy = _.reverse(_.sortBy(games_copy, ['name']));
+        }
+        console.log(games_copy)
+        return games_copy
+    }
+
     return (
         <>
         <div className='controlBox'>
             <h2>{t('gameList.h2')}</h2>
+            <div className='sortOptions'>
+                <p>Sort by:</p>
+                <div>
+                    <button className={`${sortMethod === "new" ? "active" : ""} left`} onClick={()=>{setSortMethod("new")}}>Newest</button>
+                    <button className={`${sortMethod === "old" ? "active" : ""} right`} onClick={()=>{setSortMethod("old")}}>Oldest</button>
+                </div>
+                <div>
+                    <button className={`${sortMethod === "az" ? "active" : ""} left`} onClick={()=>{setSortMethod("az")}}>{"A->Z"}</button>
+                    <button className={`${sortMethod === "za" ? "active" : ""} right`} onClick={()=>{setSortMethod("za")}}>{"Z->A"}</button>
+                </div>
+                <div>
+                    <button className={`${sortMethod === "leastTime" ? "active" : ""} left`} onClick={()=>{setSortMethod("leastTime")}}>Least playing time</button>
+                    <button className={`${sortMethod === "mostTime" ? "active" : ""} right`} onClick={()=>{setSortMethod("mostTime")}}>Most playing time</button>
+                </div>
+                <div>
+                    <button className={`${sortMethod === "leastComplex" ? "active" : ""} left`} onClick={()=>{setSortMethod("leastComplex")}}>Easiest</button>
+                    <button className={`${sortMethod === "mostComplex" ? "active" : ""} right`} onClick={()=>{setSortMethod("mostComplex")}}>Most difficult</button>
+                </div>
+            </div>
             <div>
                 <button><Link to='/new-game'>{t('gameList.add')} {plus}</Link></button>
             </div>
         </div>
         <div className='itemsList'>
             <ul>
-                {games ? games.map(game => 
+                {games ? gamesToMap().map(game => 
                     <li key={game.id}>
                         <div className='img'>
                             <img src={game.url ? game.url : noCoverSrc} alt='Cover' ></img>
