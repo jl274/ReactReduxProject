a6d6ae1821c1ffe5c1e274ce42c94819ab837764
Jakub Lendzinski <j.lendzinski.390@studms.edu.ug.pl>
2021-12-30 14:01:37 +0100

    Stats producers by country

--- projekt/webapp/src/ui/stats/Stats.js
+++ projekt/webapp/src/ui/stats/Stats.js
@@ -1,23 +1,47 @@
 import { connect } from 'react-redux';
 import { getAllGames } from '../../ducks/games/selectors';
 import _ from 'lodash';
-import { VictoryBar, VictoryChart, VictoryAxis, VictoryTheme } from 'victory';
+import { VictoryBar, VictoryChart, VictoryAxis, VictoryTheme, VictoryPie } from 'victory';
 import '../../styles/Stats.scss';
+import { getAllProducers } from '../../ducks/producers/selectors';
 
-const Stats = ({games}) => {
+const Stats = ({games, producers}) => {
 
     const gameWithMostOffers = () => {
         const game_copy = [...games.map(game => {return {"name": game.name, "offers": game.offers.length}})];
         return _.chain(game_copy).orderBy(['offers']).reverse().value().slice(0, 4);
         
     }
-    // console.log(gameWithMostOffers())
+    
+    const gamesAvgComplexity = () => {
+        const stats = {"0-20": 0, "20-40": 0, "40-60": 0, "60-80": 0, "80-100": 0};
+        games.forEach(game => {
+            if (parseInt(game.complexity) <= 20){
+                stats["0-20"] += 1
+            } else if (parseInt(game.complexity) <= 40){
+                stats["20-40"] += 1
+            } else if (parseInt(game.complexity) <= 60){
+                stats["40-60"] += 1
+            } else if (parseInt(game.complexity) <= 80){
+                stats["60-80"] += 1
+            } else {
+                stats["80-100"] += 1
+            }
+        })
+        return Object.entries(stats).map(x => {return {"x": x[0], "y": x[1]} }).filter(x => x.y !== 0);
+    }
+    
+    const countCountries = () => {
+        const producers_copy = [...producers.map(producer => {return {name: producer.name, country: producer.country}})]
+        return Object.entries(_.groupBy(producers_copy, 'country')).map(entry => {return {country: entry[0], count: entry[1].length}});
+    }
+
     return (
         <div className='stats'>
             <ul>
                 <li>
                     <h3>Most offers on game</h3>
-                    <VictoryChart
+                    {gameWithMostOffers() ? <VictoryChart
                         theme={VictoryTheme.material}
                         domainPadding={20}
                         padding={{ top: 20, bottom: 40, left: 50, right: 50 }}
@@ -40,9 +64,53 @@ const Stats = ({games}) => {
                         />
                         <VictoryBar
                             style={{ data: { fill: "#7F4677" } }}
-                            data={gameWithMostOffers()}
+                            data={gameWithMostOffers() ? gameWithMostOffers() : {}}
                             x="name"
                             y="offers"
+                        />
+                    </VictoryChart>
+                    : null
+                    }
+                </li>
+                <li>
+                    <h3>Average complexity</h3>
+                    <VictoryPie
+                        colorScale={["#7F1677", "#a6409b", "#cd65c1", "#f68be8", "#ffb2ff" ]}
+                        labels={({ datum }) => `"${datum.x}/100" ${datum.y}`}
+                        theme={VictoryTheme.material}
+                        height={250}
+                        width={500}
+                        data={gamesAvgComplexity()}
+                    />
+                </li>
+                <li>
+                    <h3>Producers by country</h3>
+                    <VictoryChart
+                        theme={VictoryTheme.material}
+                        domainPadding={40}
+                        // padding={{ top: 20, bottom: 40, left: 50, right: 50 }}
+                        height={250}
+                        width={500}
+                        // style={{margin: '100px'}}
+                        animate={{
+                            duration: 2000,
+                            onLoad: { duration: 1000 }
+                        }}
+                    >
+                        <VictoryAxis
+                            tickValues={[1, 2, 3, 4]}
+                            tickFormat={x => `${x}`}
+                            
+                        />
+                        <VictoryAxis
+                            dependentAxis
+                            tickFormat={(x) => (`${x}`)}
+                        />
+                        <VictoryBar
+                            style={{ data: { fill: "#7F4677" } }}
+                            data={countCountries() ? countCountries() : {}}
+                            x="country"
+                            y="count"
                         />
                     </VictoryChart>
                 </li>
@@ -53,7 +121,8 @@ const Stats = ({games}) => {
 
 const mapStateToProps = (state) => {
     return {
-        games: getAllGames(state)
+        games: getAllGames(state),
+        producers: getAllProducers(state)
     }
 }
 
