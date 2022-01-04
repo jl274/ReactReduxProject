e81e8d35b2524f5e17e2f509023bf9513e4f5b88
Jakub Lendzinski <j.lendzinski.390@studms.edu.ug.pl>
2022-01-04 13:05:28 +0100

    Basic producer filters

--- projekt/webapp/src/ui/producers/ProducersList.js
+++ projekt/webapp/src/ui/producers/ProducersList.js
@@ -4,11 +4,13 @@ import '../../styles/ProducersList.scss';
 import { useTranslation } from 'react-i18next';
 import { Link, withRouter } from "react-router-dom";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faPlus } from '@fortawesome/free-solid-svg-icons';
+import { faPlus, faSearch } from '@fortawesome/free-solid-svg-icons';
 import _ from 'lodash';
 import { useState } from 'react';
+import { Formik, Form, Field } from 'formik';
 
-const plus = <FontAwesomeIcon icon={faPlus} />
+const plus = <FontAwesomeIcon icon={faPlus} />;
+const searchIcon = <FontAwesomeIcon icon={faSearch} />;
 
 const ProducersList = ({producers, history}) => {
 
@@ -19,6 +21,7 @@ const ProducersList = ({producers, history}) => {
     };
 
     const [sortMethod, setSortMethod] = useState("new");
+    const [filterOptions, setFilterOptions] = useState({country: "All", after: "All"});
 
     const producersToMap = () => {
         let producers_copy = [...producers];
@@ -32,6 +35,13 @@ const ProducersList = ({producers, history}) => {
             producers_copy = _.sortBy(producers_copy, ['name']);
         } else if (sortMethod === "za"){
             producers_copy = _.reverse(_.sortBy(producers_copy, ['name']));
+        }
+
+        if (filterOptions.country !== "All"){
+            producers_copy = producers_copy.filter(x => x.country === filterOptions.country)
+        }
+        if (filterOptions.after !== "All"){
+            producers_copy = producers_copy.filter(x => (new Date(x.established)).getTime() > (new Date(filterOptions.after)).getTime())
         }
         return producers_copy
     }
@@ -55,10 +65,45 @@ const ProducersList = ({producers, history}) => {
                         <button className={`${sortMethod === "newCompany" ? "active" : ""} right`} onClick={()=>{setSortMethod("newCompany")}}>Established at the latest</button>
                     </div>
                 </div>
+
+                <div className='filterOptions'>
+                <p>{t('gameList.filters.title')}</p>
+                <Formik
+                    initialValues={{country: "All", after: "All"}}
+                    onSubmit={values=>setFilterOptions({
+                        ...filterOptions, 
+                        country: values.country,
+                        after: values.after ? values.after : "All"
+                    })}
+                >
+                    {() => 
+                        <Form id='filtersProducers'>
+                            <div className='producersBox'>
+                                <label>Country</label>
+                                <Field as="select" name="country">
+                                    <option value="All">All</option>
+                                    {producers ? _.uniqBy(producers, 'country').map(producer => 
+                                        <option key={producer.country} value={producer.country}>{producer.country}</option>) : null
+                                    }
+                                </Field>
+                            </div>
+                            <div className="producersBox">
+                                <label>Established after</label>
+                                <Field name="after" type="date"></Field>
+                            </div>
+                        </Form>
+                    }
+                </Formik>
+                <div className='submit'>
+                    <button type="submit" form='filtersProducers'>{t('gameList.filters.use')} {searchIcon}</button>
+                    <button type="reset" form='filtersProducers' onClick={()=>{setFilterOptions({country: "All", after: "All"})}}>Reset</button>
+                </div>
+            </div>
+
                 <div>
                     <button><Link to='/producers/new'>{t('producersList.add')}{plus}</Link></button>
                 </div>
-                {producers ? producersToMap().map(producer => 
+                {producers && producersToMap().length !== 0 ? producersToMap().map(producer => 
                     <li 
                         key={producer.id} id={producer.id} 
                         aria-label={`Click for details`} data-tooltip="left"
@@ -66,7 +111,7 @@ const ProducersList = ({producers, history}) => {
                     >
                         <p>{producer.name}</p>
                     </li>
-                ) : null}
+                ) : <p style={{"paddingBottom": "20px"}}>Producers not found</p>}
             </ul>
         </div>
     )
