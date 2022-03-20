ae971ec6e347528b25d406d857fcfd830bdd6feb
jl274 <94045122+jl274@users.noreply.github.com>
2022-03-20 19:10:03 +0100

    Delete lab04 directory

--- lab04/src/actions/togglerActions.js
+++
@@ -1,6 +0,0 @@
-export const typeShowToggle = 'SHOW';
-export const typeHideToggle = 'HIDE';
-
-
-export const showToggle = id => ({type: typeShowToggle, payload: id});
-export const hideToggle = id => ({type: typeHideToggle, payload: id});
