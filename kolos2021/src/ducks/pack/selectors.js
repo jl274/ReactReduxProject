55f72862d48c0b32a6369c8bb9c9bd2ff802e3df
jl274 <94045122+jl274@users.noreply.github.com>
2022-03-20 19:10:35 +0100

    Delete kolos2021 directory

--- kolos2021/src/ducks/pack/selectors.js
+++
@@ -1,2 +0,0 @@
-export const getAllPackList = (state) => state.packs;
-export const getPackById = (state, id) => state.packs.find(x => `${x.id}` === `${id}`);
--- kolos2021/src/ducks/pack/types.js
+++
@@ -1,5 +0,0 @@
-export const types = {
-    ADD: "ADD_PACK",
-    DELETE: "DELETE_PACK",
-    EDIT: "EDIT_PACK"
-}
